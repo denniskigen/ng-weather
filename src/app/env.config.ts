603399e235e71e0719268a7fb5ff314107d4e65a
Dennis Kigen <kigen.work@gmail.com>
2018-10-25 09:31:38 +0300

    Fixed API keys

--- src/app/env.config.ts
+++ src/app/env.config.ts
@@ -1,5 +1,5 @@
 export const ENV = {
   API_URL: 'https://api.openweathermap.org/data/2.5/',
-  // API_KEY: '6cfa1f8c973de554a8b8454131648c58',
-  API_KEY: 'b13f3391b2e8e324da5c024918ebbf01'
+  API_KEY: 'baedc2f2f31b7b3303e5d42d88d283c3',
+  iconUrl: 'https://openweathermap.org/img/w/'
 };
