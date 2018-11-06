ecf8c988ed52f0f183087daf7d7404e9fcb2f636
Dennis Kigen <kigen.work@gmail.com>
2018-11-06 20:06:46 +0300

    Refactored weather data retrieval methods
    getFiveDayForecast now maps the response into an array of CurrentWeatherData Items. Parsing the five-day weather response to get one result for each day is now handled in the service instead of the controller.

--- src/app/weather.service.spec.ts
+++ src/app/weather.service.spec.ts
@@ -1,12 +1,126 @@
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { asyncData, asyncError } from '../testing/async-observable-helpers';
+
+import { Weather } from './weather';
 import { WeatherService } from './weather.service';
 
-describe('WeatherService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('WeatherService (with spies)', () => {
+  let httpClientSpy: { get: jasmine.Spy };
+  let weatherService: WeatherService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ]
+  });
+  });
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    weatherService = new WeatherService(<any>httpClientSpy);
+  });
 
   it('should be created', () => {
     const service: WeatherService = TestBed.get(WeatherService);
     expect(service).toBeTruthy();
   });
+
+  it('should return expected weather (HttpClient called once)', () => {
+    const expectedWeather: Weather = {
+      city: 'Eldoret',
+      country: 'KE',
+      date: 1539885600,
+      humidity: 100,
+      icon_id: 803,
+      image: '04n',
+      temperature: 288.15,
+      description: 'broken clouds',
+      wind_speed: 1.5,
+      condition: 200
+    };
+
+    httpClientSpy.get.and.returnValue(asyncData(expectedWeather));
+
+    weatherService.getCurrentWeather(this.city).subscribe(weather =>
+      expect(weather).toEqual(expectedWeather, 'expected weather'), fail);
+
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
+
+  it('should return an error when the server returns a 404', () => {
+    const errorResponse = new HttpErrorResponse({
+      error: 'test 404 error',
+      status: 404, statusText: 'Not Found'
+    });
+
+    httpClientSpy.get.and.returnValue(asyncError(errorResponse));
+
+      weatherService.getCurrentWeather(this.city).subscribe(
+        weather => fail('expected an error, not weather'),
+        error => expect(error.message).toContain('test 404 error')
+      );
+  });
 });
+
+describe('WeatherService (with mocks)', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let weatherService: WeatherService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ WeatherService ]
+    });
+
+    httpClient = TestBed.get(httpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    weatherService = TestBed.get(WeatherService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+});
+
+
+// import { TestBed } from '@angular/core/testing';
+
+// import { Weather } from './weather';
+// import { WeatherService } from './weather.service';
+
+// describe('WeatherService', () => {
+//   let service: WeatherService;
+//   let httpClientSpy: { get: jasmine.Spy };
+//   const city = 'Eldoret';
+
+//   beforeEach(() => {
+//     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+//     service = new WeatherService(<any>httpClientSpy);
+//     TestBed.configureTestingModule({
+//       providers: [ WeatherService ]
+//     });
+//   });
+// });
+
+
+//   it('#getWeather should return a real value', () => {
+//     const result = service.getCurrentWeather(city);
+//     expect(result).toBe('real value');
+//   });
+
+//   it('#getWeather should return an observable of weather', () => {
+//     service.getCurrentWeather(city).subscribe(weather => {
+//       expect(weather).toBe('observable value');
+//     });
+//   });
+
+//   it('#getWeather should return expected weather (HttpClient called once)', () => {
+//     const expectedWeather: Weather[] = [
+      
+//     ]
+//   });
+// });
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -6,12 +6,13 @@ import { catchError, map } from 'rxjs/operators';
 import { ENV } from './env.config';
 import { Weather } from './weather';
 
-export interface CurrentWeatherData {
+interface CurrentWeatherData {
   weather: [
     {
       description: string;
-      icon: string;
       id: number;
+      icon: string;
+      main: string;
     }
   ];
   wind: {
@@ -19,6 +20,8 @@ export interface CurrentWeatherData {
   };
   main: {
     temp: number;
+    temp_min: number;
+    temp_max: number;
     humidity: number;
   };
   sys: {
@@ -33,7 +36,9 @@ export interface CurrentWeatherData {
   providedIn: 'root'
 })
 export class WeatherService {
-  constructor(private http: HttpClient) {}
+  forecast: any[] = [];
+  constructor(
+    private http: HttpClient) {}
 
   getCurrentWeather(city: string): Observable<Weather> {
     return this.http
@@ -46,30 +51,40 @@ export class WeatherService {
       );
   }
 
-  getFiveDayForecast(city: string) {
+  getFiveDayForecast(city: string): Observable<Weather[]> {
     return this.http
-      .get<any[]>(
+      .get<CurrentWeatherData[]>(
         `${ENV.API_URL}forecast/?q=${city}&units=metric&APPID=${
           ENV.API_KEY
         }`
       )
       .pipe(
-        map((data: any[]) => {
-          return data;
+        map((data: any) => {
+          this.forecast = [];
+          for (let i = 0; i < data.list.length; i += 8) {
+            this.forecast.push(data.list[i + 4]);
+          }
+          return this.forecast;
         }),
         catchError(this.handleError)
       );
   }
 
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      console.error('An error occurred:', error.error.message);
+  private handleError(error: Error | HttpErrorResponse) {
+    if (error instanceof HttpErrorResponse) {
+      // Server or connection error
+      if (!navigator.onLine) {
+        // Check your internet connection
+        console.error('It seems like you\'re not connected to the internet');
+      } else {
+        // Handle HTTP error (error.status === 403, 404, 500...)
+        console.error(`${error.status} - ${error.message}`);
+      }
     } else {
-      console.error(
-        `Server returned code ${error.status}, ` + `body was: ${error.error}`
-      );
+      // It's likely something wrong on the client-side
+      console.error('Oops', error);
     }
-    return throwError(`Something bad happened; please try again later.`);
+    return throwError('An error occurred. Please try again.');
   }
 
   private mapToWeather(data: CurrentWeatherData): Weather {
@@ -79,7 +94,7 @@ export class WeatherService {
       date: data.dt * 1000,
       humidity: data.main.humidity,
       icon_id: data.weather[0].id,
-      image: `https://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+      image: `${ENV.iconUrl}${data.weather[0].icon}.png`,
       temperature: data.main.temp,
       description: data.weather[0].description,
       wind_speed: Math.round(data.wind.speed * 3.6), // convert from m/s to km/h
