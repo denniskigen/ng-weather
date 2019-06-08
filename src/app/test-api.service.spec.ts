3b402e246368170b69e3c8bca2600235cee3be88
Dennis Kigen <kigen.work@gmail.com>
2019-06-08 18:25:11 +0300

    Fix: add HttpClientTestingModule as import dependency

--- src/app/test-api.service.spec.ts
+++ src/app/test-api.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { TestApiService } from './test-api.service';
 
 describe('TestApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ]
+  }));
 
   it('should be created', () => {
     const service: TestApiService = TestBed.get(TestApiService);
--- src/app/weather/test-weather.service.spec.ts
+++ src/app/weather/test-weather.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { TestWeatherService } from './test-weather.service';
 
 describe('TestWeatherService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ]
+  }));
 
   it('should be created', () => {
     const service: TestWeatherService = TestBed.get(TestWeatherService);
