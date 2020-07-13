720c7cffb518a4b3cfa253ad3f8adbf86a27ba7b
Dennis Kigen <kigen.work@gmail.com>
2020-07-13 21:38:03 +0300

    Reorder WeatherService spec mocks

--- src/app/weather.service.spec.ts
+++ src/app/weather.service.spec.ts
@@ -8,96 +8,6 @@ import { Weather } from './weather';
 import { WeatherService } from './weather.service';
 import { mockCurrentWeather, mockFiveDayForecast } from './weather.mock';
 
-const mappedCurrentWeather: Weather = {
-  city: 'Eldoret',
-  country: 'KE',
-  date: 1559675580000,
-  humidity: 88,
-  icon: '04n',
-  icon_id: 803,
-  image: 'https://openweathermap.org/img/w/04n.png',
-  temperature: 17,
-  description: 'broken clouds',
-  wind_speed: 4,
-  condition: 200,
-  max: 22,
-  min: 13
-};
-
-const mappedFiveDayForecast: Weather[] = [
-  {
-    date: 1559725200000,
-    description: 'few clouds',
-    dt_txt: '2019-06-05 09:00:00',
-    humidity: 67,
-    icon: '02d',
-    icon_id: 801,
-    image: 'https://openweathermap.org/img/w/02d.png',
-    max: 21.53,
-    min: 12.98,
-    temperature: 21.53,
-    wind_speed: 10
-  },
-  {
-    date: 1559811600000,
-    description: 'few clouds',
-    dt_txt: '2019-06-06 09:00:00',
-    humidity: 60,
-    icon: '02d',
-    icon_id: 801,
-    image: 'https://openweathermap.org/img/w/02d.png',
-    max: 22.28,
-    min: 13.44,
-    temperature: 22.28,
-    wind_speed: 8
-  },
-  {
-    date: 1559898000000,
-    description: 'scattered clouds',
-    dt_txt: '2019-06-07 09:00:00',
-    humidity: 62,
-    icon: '03d',
-    icon_id: 802,
-    image: 'https://openweathermap.org/img/w/03d.png',
-    max: 22.65,
-    min: 13.41,
-    temperature: 22.65,
-    wind_speed: 7
-  },
-  {
-    date: 1559984400000,
-    description: 'light rain',
-    dt_txt: '2019-06-08 09:00:00',
-    humidity: 68,
-    icon: '10d',
-    icon_id: 500,
-    image: 'https://openweathermap.org/img/w/10d.png',
-    max: 21.36,
-    min: 12.88,
-    temperature: 21.36,
-    wind_speed: 7
-  },
-  {
-    date: 1560070800000,
-    description: 'broken clouds',
-    dt_txt: '2019-06-09 09:00:00',
-    humidity: 66,
-    icon: '04d',
-    icon_id: 803,
-    image: 'https://openweathermap.org/img/w/04d.png',
-    max: 21.27,
-    min: 12.77,
-    temperature: 21.27,
-    wind_speed: 6
-  }
-];
-
-const mockErrorResponse = new HttpErrorResponse({
-  error: 'Internal Server Error',
-  status: 500,
-  statusText: 'An internal server error occurred'
-});
-
 describe('WeatherService ', () => {
   let httpClientSpy: { get: jasmine.Spy };
   let service: WeatherService;
@@ -213,4 +123,95 @@ describe('WeatherService ', () => {
       }
     );
   });
+});
+
+
+const mappedCurrentWeather: Weather = {
+  city: 'Eldoret',
+  country: 'KE',
+  date: 1559675580000,
+  humidity: 88,
+  icon: '04n',
+  icon_id: 803,
+  image: 'https://openweathermap.org/img/w/04n.png',
+  temperature: 17,
+  description: 'broken clouds',
+  wind_speed: 4,
+  condition: 200,
+  max: 22,
+  min: 13
+};
+
+const mappedFiveDayForecast: Weather[] = [
+  {
+    date: 1559725200000,
+    description: 'few clouds',
+    dt_txt: '2019-06-05 09:00:00',
+    humidity: 67,
+    icon: '02d',
+    icon_id: 801,
+    image: 'https://openweathermap.org/img/w/02d.png',
+    max: 21.53,
+    min: 12.98,
+    temperature: 21.53,
+    wind_speed: 10
+  },
+  {
+    date: 1559811600000,
+    description: 'few clouds',
+    dt_txt: '2019-06-06 09:00:00',
+    humidity: 60,
+    icon: '02d',
+    icon_id: 801,
+    image: 'https://openweathermap.org/img/w/02d.png',
+    max: 22.28,
+    min: 13.44,
+    temperature: 22.28,
+    wind_speed: 8
+  },
+  {
+    date: 1559898000000,
+    description: 'scattered clouds',
+    dt_txt: '2019-06-07 09:00:00',
+    humidity: 62,
+    icon: '03d',
+    icon_id: 802,
+    image: 'https://openweathermap.org/img/w/03d.png',
+    max: 22.65,
+    min: 13.41,
+    temperature: 22.65,
+    wind_speed: 7
+  },
+  {
+    date: 1559984400000,
+    description: 'light rain',
+    dt_txt: '2019-06-08 09:00:00',
+    humidity: 68,
+    icon: '10d',
+    icon_id: 500,
+    image: 'https://openweathermap.org/img/w/10d.png',
+    max: 21.36,
+    min: 12.88,
+    temperature: 21.36,
+    wind_speed: 7
+  },
+  {
+    date: 1560070800000,
+    description: 'broken clouds',
+    dt_txt: '2019-06-09 09:00:00',
+    humidity: 66,
+    icon: '04d',
+    icon_id: 803,
+    image: 'https://openweathermap.org/img/w/04d.png',
+    max: 21.27,
+    min: 12.77,
+    temperature: 21.27,
+    wind_speed: 6
+  }
+];
+
+const mockErrorResponse = new HttpErrorResponse({
+  error: 'Internal Server Error',
+  status: 500,
+  statusText: 'An internal server error occurred'
 });
