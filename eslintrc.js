8bbff3743f1fcd8698e329e2dc6ae0d7b0920f27
Dennis Kigen <kigen.work@gmail.com>
2020-07-30 13:18:27 +0300

    Lint all the things; not just src/app

--- eslintrc.js
+++ eslintrc.js
@@ -5,6 +5,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint'
-  ]
+    'prettier/@typescript-eslint',
+  ],
 };
